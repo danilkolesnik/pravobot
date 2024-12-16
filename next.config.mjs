d0d0a987a38ff89df25df799fd55007eaa5aa657
danilkolesnik <apparentlysolved@gmail.com>
2024-12-16 12:16:18 +0200

    edit config

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,6 @@
 
 const nextConfig = {
     images: { unoptimized: true },
-    // webpack: (config) => {
-    //   config.module.rules.push({
-    //     test: /\.(woff|woff2|eot|ttf|otf)$/,
-    //     use: {
-    //       loader: 'file-loader',
-    //       options: {
-    //         name: '[name].[ext]',
-    //         outputPath: 'static/fonts/',
-    //         publicPath: '/_next/static/fonts/',
-    //         esModule: false,
-    //       },
-    //     },
-    //   });
-  
-    //   return config;
-    // },
   };
   
   export default nextConfig;
