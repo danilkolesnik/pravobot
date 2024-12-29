795605d89f93349105755e16be6af99b5d3130ae
danilkolesnik <apparentlysolved@gmail.com>
2024-12-30 00:49:44 +0200

    add: static

---
+++ src/app/api/pages/route.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+import { URL } from '@component/utils/constants';
+
+export const dynamic = 'force-static';
+
+export async function GET() {
+    try {
+        const response = await axios.get(`${URL}/api/pages`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        });
+        return NextResponse.json(response.data);
+    } catch (error) {
+        console.error('Error:', error.response?.data || error.message);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
